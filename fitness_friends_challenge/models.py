0db7b9f4eb219aae536aa3e44a63e3f2fbacdc40
Bryce Koenig <brycek30@gmail.com>
2021-07-17 02:30:01 -0700

    got db to work and insert and display user

--- fitness_friends_challenge/models.py
+++ fitness_friends_challenge/models.py
@@ -1,24 +1,29 @@
 from fitness_friends_challenge import db
 
 challenge_badges = db.Table('challengebadges',
-    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id'), primary_key=True),
-    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id'), primary_key=True))
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id')),
+    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id')))
 
 user_badges = db.Table('userbadges',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id'), primary_key=True))
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id')))
 
 challenge_chats = db.Table('challengechats',
-    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id'), primary_key=True),
-    db.Column('chat_id', db.Integer, db.ForeignKey('chat.id'), primary_key=True))
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id')),
+    db.Column('chat_id', db.Integer, db.ForeignKey('chat.id')))
 
 challenge_tags = db.Table('challengetags',
-    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True))
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')))
 
 challenge_images = db.Table('challengeimages',
-    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id'), primary_key=True),
-    db.Column('image_id', db.Integer, db.ForeignKey('image.id'), primary_key=True))
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id')),
+    db.Column('image_id', db.Integer, db.ForeignKey('image.id')))
 
 class Challenge(db.Model):
     __tablename__ = 'challenge'
@@ -34,12 +39,6 @@ class Challenge(db.Model):
     chats = db.relationship('Chat', secondary=challenge_chats, lazy='dynamic')
     images = db.relationship('Image', secondary=challenge_images, lazy='dynamic')
 
-    def __init__(self, name):
-        self.name = name
-
-    def convert_to_json(self):
-        return {'name': self.name}
-
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -50,19 +49,15 @@ class User(db.Model):
     email = db.Column(db.Text(), nullable=False)
     images = db.relationship('Image', lazy='dynamic')
     badges = db.relationship('Badge', secondary=user_badges, lazy='dynamic')
-    challenges = db.relationship('Challenge', lazy='dynamic')
     chats = db.relationship('Chat', lazy='dynamic')
-
-    def convert_to_json(self):
-        return {'username': self.username, 'password': self.password,
-                'firstname': self.firstname, 'lastname': self.lastname,
-                'email': self.email}
+    favorites = db.relationship('Favorites', lazy='dynamic')
+    #walloffame_id = db.Column(db.Integer, db.ForeignKey('walloffame.id'))
 
 class Badge(db.Model):
     __tablename__ = 'badge'
     id = db.Column(db.Integer, primary_key=True)
-    users = db.relationship('User', lazy='dynamic')
-    challenges = db.relationship('Challenge', lazy='dynamic')
+    users = db.relationship('User', secondary=user_badges, lazy='dynamic')
+    challenges = db.relationship('Challenge', secondary=challenge_badges, lazy='dynamic')
     image = db.Column(db.Text(), nullable=False)
 
 class Goal(db.Model):
@@ -70,7 +65,7 @@ class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(), nullable=False)
     description = db.Column(db.Text(), nullable=False)
-    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge_id'))
+    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge.id'))
 
 class Kind(db.Model):
     __tablename__ = 'kind'
@@ -82,26 +77,29 @@ class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(), nullable=False)
+    challenges = db.relationship('Challenge', secondary=challenge_tags, lazy='dynamic')
 
 class Chat(db.Model):
     __tablename__ = 'chat'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    challenges = db.relationship('Challenge', secondary=challenge_chats, lazy='dynamic')
 
 class Image(db.Model):
     __tablename__ = 'image'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    challenges = db.relationship('Challenge', secondary=challenge_images, lazy='dynamic')
 
 class Favorites(db.Model):
     __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     challenges = db.relationship('Challenge', lazy='dynamic')
 
 class WallOfFame(db.Model):
     __tablename__ = 'walloffame'
     id = db.Column(db.Integer, primary_key=True)
-    users = db.relationship('User', lazy='dynamic')
-    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge_id'))
+    #users = db.relationship('User', lazy='dynamic')
+    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge.id'))
--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-
+from sqlalchemy import engine_from_config, pool
 import logging
 from logging.config import fileConfig
 
@@ -20,10 +20,10 @@ logger = logging.getLogger('alembic.env')
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+print(current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 config.set_main_option(
     'sqlalchemy.url',
-    str(current_app.extensions['migrate'].db.get_engine().url).replace(
-        '%', '%%'))
+    current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -34,15 +34,12 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -55,10 +52,8 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
@@ -71,18 +66,23 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = current_app.extensions['migrate'].db.get_engine()
+    engine = engine_from_config(config.get_section(config.config_ini_section),
+                                prefix='sqlalchemy.',
+                                poolclass=pool.NullPool)
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            process_revision_directives=process_revision_directives,
-            **current_app.extensions['migrate'].configure_args
-        )
+    connection = engine.connect()
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        process_revision_directives=process_revision_directives,
+        **current_app.extensions['migrate'].configure_args
+    )
 
+    try:
         with context.begin_transaction():
             context.run_migrations()
+    finally:
+        connection.close()
 
 
 if context.is_offline_mode():
