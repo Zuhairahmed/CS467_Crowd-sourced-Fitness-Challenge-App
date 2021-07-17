26ab995fe09aa453e59c855e429df14b85dd7c57
Zuhairahmed <ahmedz@oregonstate.edu>
2021-07-16 23:13:38 -0700

    Update fitness_friends_challenge.py

--- fitness_friends_challenge/fitness_friends_challenge.py
+++ fitness_friends_challenge/fitness_friends_challenge.py
@@ -6,54 +6,67 @@ from fitness_friends_challenge.models import Challenge, User, Badge, Favorites, 
 bp = Blueprint('fitness_friends_challenge', __name__)
 
 
-
-#Default route displays Home/Landing page
+# Default route displays Home/Landing page
 @bp.route('/')
 def index():
     return render_template('index.html')
 
-#Route for user registration page
+# Route for user registration page
+
+
 @bp.route('/registration')
 def registration():
     return render_template('registration.html')
 
-#Route for user profile page
-#Note: this route's url will change to the username once logged in, when that is set up
+# Route for user profile page
+# Note: this route's url will change to the username once logged in, when that is set up
+
+
 @bp.route('/usernamehere/profile')
 def userprofile():
     return render_template('profile.html')
 
-#Route for challenge search results page
-#Note: this route's url will change to include tags used for search when set up
+# Route for challenge search results page
+# Note: this route's url will change to include tags used for search when set up
+
+
 @bp.route('/challenges/search')
 def challenges():
     return render_template('search.html')
 
-#Route for create challenge page
+# Route for create challenge page
+
+
 @bp.route('/challenges/create')
 def createchallenge():
     return render_template('create.html')
 
-#Route for challenge home page
-#Note: this route's url will change to include challenge name when set up
+# Route for challenge home page
+# Note: this route's url will change to include challenge name when set up
+
+
 @bp.route('/challenges/challengenamehere')
 def challengehome():
     return render_template('challenge.html')
 
-#Route for challenge wall of fame page
-#Note: this route's url will change to include challenge name when set up
+# Route for challenge wall of fame page
+# Note: this route's url will change to include challenge name when set up
+
+
 @bp.route('/challenges/challengenamehere/wall-of-fame')
 def walloffame():
     return render_template('wall.html')
 
-#Route for about us page
+# Route for about us page
+
+
 @bp.route('/about-us')
 def about():
     return render_template('about.html')
 
-#Route for faq page
+# Route for faq page
+
+
 @bp.route('/learn-more')
 def faq():
     return render_template('faq.html')
-
-
