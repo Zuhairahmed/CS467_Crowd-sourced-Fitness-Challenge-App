dbfdb1e07a6c26cedfde77843b7f79874ede6beb
distefab <distefab@oregonstate.edu>
2021-07-31 01:45:35 -0700

    Validates passwords by flashing the top of the screen.

--- fitness_friends_challenge/forms.py
+++ fitness_friends_challenge/forms.py
@@ -1,6 +1,7 @@
 from wtforms import Form, StringField, PasswordField, validators, ValidationError
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, email, email_validator
 from flask import flash
+import re
 
 
 def flash_errors(form):
@@ -12,18 +13,28 @@ def flash_errors(form):
                 error
             ), 'error')
 
-def checkUsername(form, field):
-    if len(field.data) < 4:
-        raise ValidationError('Username must be at least 4 characters long.')
-    if len(field.data) > 12:
-        raise ValidationError('Username must be shorter than 12 characters.')
+def checkPassword(form, field):
+    if len(field.data) < 8:
+        raise ValidationError("Password must be at least 8 characters long.")
+    elif len(field.data) > 32:
+        raise ValidationError("Password can't be bigger than 32 characters long.")
+    elif re.search('[0-9]',field.data) is None:
+        raise ValidationError("Your password must have a number in it.")
+    elif re.search('[A-Z]',field.data) is None:
+        raise ValidationError("Your password must have a capital letter in it.")
+
+
+
+
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', [InputRequired(), checkUsername])
-    password = PasswordField('New Password', [
-        validators.DataRequired()
-    ])
+    username = StringField('Username', [InputRequired(message="Please enter a username"), validators.Length(min=3, max=10)])
+    password = PasswordField('New Password', [validators.DataRequired(message="Please enter a password."), checkPassword])
     firstname = StringField('First Name', [validators.data_required()])
     lastname = StringField('Last Name', [validators.data_required()])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    email = StringField('Email Address', [email(), validators.Length(min=6, max=35)])
+
+
+
+
