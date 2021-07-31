0436f53f1b31d691a40ae377b2baaeccd2c1d401
distefab <distefab@oregonstate.edu>
2021-07-31 08:05:36 -0700

    went back

--- fitness_friends_challenge/__init__.py
+++ fitness_friends_challenge/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
         # os.environ.get('DATABASE_URL')
-        SQLALCHEMY_DATABASE_URI = 'postgresql://jcqomyoeuvlwiy:6bd0835cfb0797bb13ef0e4548928c06d8cdb0a79b051d70312a9aea1356ba7a@ec2-54-224-194-214.compute-1.amazonaws.com:5432/d1rvvqpp8hfonq', 
+        SQLALCHEMY_DATABASE_URI = 'postgresql://jcqomyoeuvlwiy:6bd0835cfb0797bb13ef0e4548928c06d8cdb0a79b051d70312a9aea1356ba7a@ec2-54-224-194-214.compute-1.amazonaws.com:5432/d1rvvqpp8hfonq',
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 
@@ -19,7 +19,6 @@ def create_app():
     migrate.init_app(app, db)
 
     from . import models
-    from . import forms
     from . import fitness_friends_challenge
     app.register_blueprint(fitness_friends_challenge.bp)
 
--- fitness_friends_challenge/fitness_friends_challenge.py
+++ fitness_friends_challenge/fitness_friends_challenge.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# Authors: BrYan DiStefano, Bryce Koenig, Zuhair Ahmed
+# Authors: Brian DiStefano, Bryce Koenig, Zuhair Ahmed
 # Course: CS467_400_Summer 2021
 #
 # Description:
@@ -15,7 +15,6 @@
 from flask import Flask, redirect, render_template, request, url_for, session, Blueprint, flash
 from fitness_friends_challenge import db
 from fitness_friends_challenge.models import Challenge, User, Badge, Favorites, Tag, Goal, Chat, Image, WallOfFame, Kind
-from fitness_friends_challenge.forms import RegistrationForm, flash_errors
 
 
 bp = Blueprint('fitness_friends_challenge', __name__)
@@ -32,7 +31,7 @@ def index():
             return redirect(url_for('fitness_friends_challenge.userprofile', username=user.username))
     return render_template('index.html')
 
-# Admin page to create and review created users 
+# Admin page to create and review created users
 @bp.route('/users', methods=('GET', 'POST'))
 def users():
     if request.method == 'POST':
@@ -41,13 +40,13 @@ def users():
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         email = request.form['email']
-        db.session.add(User(username=username, password=password, firstname=firstname, 
+        db.session.add(User(username=username, password=password, firstname=firstname,
                             lastname=lastname, email=email, walloffame_id=None))
         db.session.commit()
     users = User.query.all()
     return render_template('users.html', users=users)
 
-# Admin page to create and review created tags 
+# Admin page to create and review created tags
 @bp.route('/tags', methods=('GET', 'POST'))
 def tags():
     if request.method == 'POST':
@@ -57,7 +56,7 @@ def tags():
     tags = Tag.query.all()
     return render_template('tags.html', tags=tags)
 
-# Admin page to create and review created badges 
+# Admin page to create and review created badges
 @bp.route('/badges', methods=('GET', 'POST'))
 def badges():
     if request.method == 'POST':
@@ -67,7 +66,7 @@ def badges():
     badges = Badge.query.all()
     return render_template('badges.html', badges=badges)
 
-# Admin page to create and review created kind of challenges 
+# Admin page to create and review created kind of challenges
 @bp.route('/kinds', methods=('GET', 'POST'))
 def kinds():
     if request.method == 'POST':
@@ -80,20 +79,17 @@ def kinds():
 # Route for user registration page
 @bp.route('/registration', methods=('GET', 'POST'))
 def registration():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         email = request.form['email']
-        db.session.add(User(username=username, password=password, firstname=firstname, 
+        db.session.add(User(username=username, password=password, firstname=firstname,
                             lastname=lastname, email=email, walloffame_id=None))
         db.session.commit()
         return redirect(url_for('fitness_friends_challenge.index'))
-    else:
-        flash_errors(form)
-    return render_template('registration.html', form=form)
+    return render_template('registration.html')
 
 # Route for user profile page
 @bp.route('/users/<username>/profile', methods=('GET', 'POST'))
